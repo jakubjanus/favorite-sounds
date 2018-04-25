18102db874247257585f0a270d75a8fdbb2bc5b2
Jakub <jakub.janus@droidsonroids.pl>
2018-04-25 12:37:21 +0200

    1. changed testing framework to RSpec 2. first acceptance test added

---
+++ spec/acceptance/adding_favorite_sound_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Adding favorite sound', type: :request do
+  describe 'Adding sound to default folder' do
+    let(:favorite_params) do
+      {
+        sound_id: 1
+      }
+    end
+
+    before do
+      post '/api/v1/favorites', params: favorite_params
+    end
+
+    subject { response }
+
+    it 'response is with 200 status' do
+      expect(subject.status).to eq(200)
+    end
+  end
+end
