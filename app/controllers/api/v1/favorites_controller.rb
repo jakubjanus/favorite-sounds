43e257bc06d5409ea4bdc4471438e06ebec8c80b
Jakub <jakub.janus@droidsonroids.pl>
2018-04-25 12:54:29 +0200

    1. established expected JSON response schema for POST /api/v1/favorites 2. changed response status to more adequate 201

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class FavoritesController < ApplicationController
       def create
+        render json: { sound_id: 1, folder_id: nil }, status: :created
       end
     end
   end
--- spec/acceptance/adding_favorite_sound_spec.rb
+++ spec/acceptance/adding_favorite_sound_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+RSpec::Matchers.define :be_valid_favorite_response do
+  match do |actual_response_json|
+    expect(actual_response_json).to have_key('sound_id')
+    expect(actual_response_json).to have_key('folder_id')
+  end
+end
+
+
 RSpec.describe 'Adding favorite sound', type: :request do
   describe 'Adding sound to default folder' do
     let(:favorite_params) do
@@ -14,8 +22,21 @@ RSpec.describe 'Adding favorite sound', type: :request do
 
     subject { response }
 
-    it 'response is with 200 status' do
-      expect(subject.status).to eq(204)
+    it 'response is with 201 status' do
+      expect(subject.status).to eq(201)
     end
+
+    it 'response body has correct schema' do
+      expect(response_json).to be_valid_favorite_response
+    end
+
+    it 'response body should include proper sound_id and folder' do
+      expect(response_json['sound_id']).to eq(1)
+      expect(response_json['folder_id']).to be_nil
+    end
+  end
+
+  def response_json
+    JSON.parse(response.body)
   end
 end
